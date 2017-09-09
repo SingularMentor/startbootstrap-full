adb08a4144aeb314bd89b41a4dfce3bf366b9dd9
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2017-09-09 12:08:03 -0300

    Adds colors, buttons and stiles.

--- gulpfile.js
+++ gulpfile.js
@@ -15,9 +15,6 @@ gulp.task('copy', function() {
 
   gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
     .pipe(gulp.dest('vendor/jquery'))
-
-  gulp.src(['node_modules/popper.js/dist/umd/popper.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-    .pipe(gulp.dest('vendor/popper'))
 })
 
 // Default task
@@ -35,6 +32,6 @@ gulp.task('browserSync', function() {
 // Dev task with browserSync
 gulp.task('dev', ['browserSync'], function() {
   // Reloads the browser whenever HTML or CSS files change
-  gulp.watch('css/*.css', browserSync.reload);
+  gulp.watch('static/css/*.css', browserSync.reload);
   gulp.watch('*.html', browserSync.reload);
 });
